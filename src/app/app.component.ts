08cc3737f40c2c430f27a3f92d4cc76005ce6052
Nasreddine <eln@outlook.be>
2025-03-09 17:09:15 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,27 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+    <header>
+      <nav class="bg-white-500 text-black border-bottom p-4">
+        <ul class="flex justify-between">
+          <li class="bold">Where in the world?</li>
+          <li>Dark Mode</li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+  `,
+  styles: `
+    header,
+    main,
+    footer {
+      padding: 0 2rem;
+    }
+  `,
 })
 export class AppComponent {
   title = 'rest-countries';
